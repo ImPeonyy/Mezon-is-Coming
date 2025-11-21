2e694a4c994dcef20734eec953658314b4bfcff1
Peonyy~ <thieuthang1412@gmail.com>
2025-11-21 17:14:47 +0700

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import {
+    ExampleCommandHandlers,
+    ExampleComponentHandlers,
+    ExampleDMHandlers,
+    ExampleEmbedHandlers,
+    ExampleMentionHandlers,
+} from "./bot/commands";
+import { ExampleEventHandlers } from "./bot/events";
+import { NezonModule } from "@n0xgg04/nezon";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+        }),
+        NezonModule.forRoot({
+            token: process.env.MEZON_TOKEN ?? "",
+            botId: process.env.MEZON_BOT_ID ?? "",
+        }),
+    ],
+    controllers: [AppController],
+    providers: [
+        AppService,
+        ExampleCommandHandlers,
+        ExampleComponentHandlers,
+        ExampleDMHandlers,
+        ExampleEmbedHandlers,
+        ExampleEventHandlers,
+        ExampleMentionHandlers,
+    ],
+})
+export class AppModule {}
---
+++ src/bot/commands/index.ts
@@ -0,0 +1,5 @@
+export * from "./example-mention.handlers";
+export * from "./example-command.handlers";
+export * from "./example-component.handlers";
+export * from "./example-dm.handlers";
+export * from "./example-embed.handlers";
