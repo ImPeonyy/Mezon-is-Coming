ab9d70c3458dacb1dc5714eebb8ae79f2526d941
Peonyy~ <thieuthang1412@gmail.com>
2026-01-31 23:20:03 +0700

    Refactor: re-init database

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -8,7 +8,7 @@ export class UsersService {
 
     async getUser(userId: string) {
         return await this.prisma.user.findUnique({
-            where: { id: userId },
+            where: { id: Number(userId) },
         });
     }
 
@@ -20,14 +20,14 @@ export class UsersService {
 
     async updateUser(userId: string, user: Prisma.UserUpdateInput) {
         return await this.prisma.user.update({
-            where: { id: userId },
+            where: { id: Number(userId) },
             data: user,
         });
     }
 
     async deleteUser(userId: string) {
         return await this.prisma.user.delete({
-            where: { id: userId },
+            where: { id: Number(userId) },
         });
     }
 }
